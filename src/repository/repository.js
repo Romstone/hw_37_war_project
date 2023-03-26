7c8fc3caedf8a65fefb9e9e79a40ee7a8548a055
Romstone <czeslawmeyerx@gmail.com>
2023-03-26 22:07:45 +0300

    Minor tweaks

--- src/repository/repository.js
+++ src/repository/repository.js
@@ -7,14 +7,21 @@ export default class DataRepository
         const responseData = await Api.getDeck();
         console.log(responseData);
         responseData.cards.forEach(item => {
-            if (item.value === 'JACK')
-                item.value = 11;
-            if (item.value === 'QUEEN')
-                item.value = 12;
-            if (item.value === 'KING')
-                item.value = 13;
-            if (item.value === 'ACE')
-                item.value = 14;
+            switch (item.value)
+            {
+                case 'JACK':
+                    item.value = 11;
+                    break;
+                case 'QUEEN':
+                    item.value = 12;
+                    break;
+                case 'KING':
+                    item.value = 13;
+                    break;
+                case 'ACE':
+                    item.value = 14;
+                    break;
+            }
         })
         return responseData.cards;
     }
